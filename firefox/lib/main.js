bd40204891400fe4658820fdd1d005d21e0f2bff
Edgar Vaguencia <isc.edgarvalencia@gmail.com>
2015-03-01 23:54:29 -0600

    :v: version 0.1.1 Cambio el código como parte "nativa" de firefox (uso correcto de sdk y no adaptado de chrome)

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -1,7 +1,9 @@
 var { ToggleButton } = require('sdk/ui/button/toggle'),
 	self = require("sdk/self"),
 	panels = require("sdk/panel"),
-	tabs = require("sdk/tabs");
+	tabs = require("sdk/tabs"),
+	_ = require('underscore'),
+	UrlSite = "http://khaleesi.unisem.mx/admin/";
 
 var button = ToggleButton({
   id: "show-panel",
@@ -16,7 +18,7 @@ var button = ToggleButton({
 
 var panel = panels.Panel({
 	contentURL: self.data.url("index.html"),
-	contentScriptFile: self.data.url("./js/app.js"),
+	contentScriptFile: self.data.url("./js/init.js"),
 	contentScriptWhen : "end",
 	onHide: handleHide
 });
@@ -26,6 +28,7 @@ function handleClick(state) {
 		panel.show({
 			position: button
 		});
+		panel.port.emit('resource');
 	}
 }
 
@@ -33,10 +36,43 @@ function handleHide() {
 	button.state('window', {checked: false});
 }
 
-panel.on("show",function(){
-	panel.port.emit("resource");
+// Empty Task
+function viewEmpty(){
+	var html = '<div class="login"><div>No tienes tareas iniciadas, accede al sistema para iniciar una y "A darle átomos"</div><span class="btn" id="newTask">Crear nueva</span></div>';
+	panel.port.emit('render',html);
+}
+
+// View Login
+panel.port.on('login',function(){
+	templateHTML = '<div class="login"><div>Identificate para poder comenzar</div><span class="btn" id="login"><%= value %></span></div>';
+	var compiled = _.template(templateHTML);
+	var html = compiled({ value: 'Login' });
+	panel.port.emit('render',html);
 });
 
-panel.port.on('newTab',function(link){
-	tabs.open(link);
+// Open Tabs
+panel.port.on('openTab',function(obj){
+	if( typeof obj === 'object' ){
+		var type = obj.name,
+			link;
+		if( obj.url ){
+			link = obj.url;
+		}else if( type == 'newTask' ){
+			link = UrlSite + 'track/tarea/add/';
+		}else{
+			link = UrlSite;
+		}
+		tabs.open(link);
+	}
+});
+
+panel.port.on('data',function(data){
+	if( data.tasks.length > 0 ){
+		console.log(data.tasks);
+	}else{
+		viewEmpty();
+	}
+	if( data.last_tasks.length > 0 ){
+		//console.log(data.last_tasks);
+	}
 });
