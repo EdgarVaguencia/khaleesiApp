2af0e22de2f29dae68eb0c7a74feb0f4e9ba1e4d
Edgar Vaguencia <isc.edgarvalencia@gmail.com>
2016-10-06 15:30:02 -0500

    Pruebas finales del uso de la api

--- chrome/js/main.js
+++ chrome/js/main.js
@@ -1,9 +1,9 @@
 var Backbone = require('backbone'),
-	Router = require('./routers/router'),
-	$ = require('jquery')
-	Backbone.$ = $,
-	UrlSite = "http://khaleesi.unisem.mx/api/v1/";
+    Router = require('./routers/router'),
+    $ = require('jquery')
+    Backbone.$ = $,
+    UrlSite = 'http://khaleesi.unisem.mx/api/v1/';
 
 $(function(){
-	 Backbone.app = new Router({ url: UrlSite });
+     Backbone.app = new Router({ url: UrlSite});
 });
